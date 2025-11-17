6e5da2e542a7c8378921106ad5f007c5bd4967ea
Vignesh Shankar S <connectwithvigneshshankar@gmail.com>
2025-11-17 11:44:47 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,22 +1,27 @@
 
-def get_color_map():
-    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-    mapping = []
-    for i, major in enumerate(major_colors):
-        for j, minor in enumerate(minor_colors):
-            mapping.append(f'{i * 5 + j} | {major} | {minor}')
-    return mapping
+def color_index_pair_number(major_color_index, minor_color_index):
+    return major_color_index * 5 + minor_color_index + 1
+
+
+def __number_of_characters_in_largest_value_in_list(input_list):
+    return max(len(str(item)) for item in input_list)
+
 
 def print_color_map():
-    for entry in get_color_map():
-        print(entry)
-    return len(get_color_map())
+    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    major_color_max_char = __number_of_characters_in_largest_value_in_list(major_colors)
+    minor_color_max_char = __number_of_characters_in_largest_value_in_list(minor_colors)
+    for i, major in enumerate(major_colors):
+        dashes_for_major = '-' * (major_color_max_char)
+        dashes_for_minor = '-' * (minor_color_max_char)
+        print(f"| -- | {dashes_for_major} | {dashes_for_minor} |")
+        for j, minor in enumerate(minor_colors):
+            index_num = color_index_pair_number(i, j)
+            print(f'| {index_num:<2} | {major:<{major_color_max_char}} | {minor:<{minor_color_max_char}} |')
+    print(f"| -- | {dashes_for_major} | {dashes_for_minor} |")
+    return len(major_colors) * len(minor_colors)
 
-# Strengthened test: check the first mapping value for correct alignment
-mapping = get_color_map()
-assert(mapping[0] == '0 | White | Blue')  # This will pass, but let's check a misaligned value
-assert(mapping[5] == '5 | Red | Blue')   # This should fail if misaligned
-result = print_color_map()
-assert(result == 25)
-print("All is well (maybe!)")
+
+if __name__ == '__main__':  # pragma: no cover
+    print_color_map()
