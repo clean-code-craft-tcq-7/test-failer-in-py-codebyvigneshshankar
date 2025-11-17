c93c6095a5de38d05bff620938cfd9337ff7bf7a
Vignesh Shankar S <connectwithvigneshshankar@gmail.com>
2025-11-17 11:19:50 +0530

    Update weatherreport.py

--- weatherreport.py
+++ weatherreport.py
@@ -18,24 +18,32 @@ def report(sensorReader):
             weather = "Partly Cloudy"
         elif readings['windSpeedKMPH'] > 50:
             weather = "Alert, Stormy with heavy rain"
+        elif readings['precipitation'] >= 60 and readings['windSpeedKMPH'] < 50:
+            weather = "Rainy Day"
     return weather
 
 
 def testRainy():
     weather = report(sensorStub)
     print(weather)
-    assert("rain" in weather)
+    assert("rain" in weather)  # This will fail if bug exists
 
 
 def testHighPrecipitation():
     # This instance of stub needs to be different-
     # to give high precipitation (>60) and low wind-speed (<50)
 
-    weather = report(sensorStub)
-
-    # strengthen the assert to expose the bug
-    # (function returns Sunny day, it should predict rain)
-    assert(len(weather) > 0);
+    def high_precip_stub():
+        return {
+            'temperatureInC': 51,
+            'precipitation': 70,  # >60
+            'humidity': 26,
+            'windSpeedKMPH': 40   # <50
+        }
+    weather = report(high_precip_stub)
+    print(weather)
+    # Strengthened assert: should predict rain, now matches 'Rainy Day'
+    assert("rain" in weather.lower())
 
 
 if __name__ == '__main__':
