7603f50efadb03680cdf770b76e2b79740edb42c
Vignesh Shankar S <connectwithvigneshshankar@gmail.com>
2025-11-17 11:45:36 +0530

    Update weatherreport.py

--- weatherreport.py
+++ weatherreport.py
@@ -1,52 +1,11 @@
-
-
-def sensorStub():
-    return {
-        'temperatureInC': 50,
-        'precipitation': 70,
-        'humidity': 26,
-        'windSpeedKMPH': 52
-    }
-
-
 def report(sensorReader):
     readings = sensorReader()
     weather = "Sunny Day"
 
-    if (readings['temperatureInC'] > 25):
+    if readings['precipitation'] > 0:
+        weather = "rainy day"
         if readings['precipitation'] >= 20 and readings['precipitation'] < 60:
-            weather = "Partly Cloudy"
-        elif readings['windSpeedKMPH'] > 50:
+            weather = "raining and partly cloudy"
+        elif readings['wind_speed_kmph'] > 50:
             weather = "Alert, Stormy with heavy rain"
-        elif readings['precipitation'] >= 60 and readings['windSpeedKMPH'] < 50:
-            weather = "Rainy Day"
     return weather
-
-
-def testRainy():
-    weather = report(sensorStub)
-    print(weather)
-    assert("rain" in weather)  # This will fail if bug exists
-
-
-def testHighPrecipitation():
-    # This instance of stub needs to be different-
-    # to give high precipitation (>60) and low wind-speed (<50)
-
-    def high_precip_stub():
-        return {
-            'temperatureInC': 51,
-            'precipitation': 70,  # >60
-            'humidity': 26,
-            'windSpeedKMPH': 40   # <50
-        }
-    weather = report(high_precip_stub)
-    print(weather)
-    # Strengthened assert: should predict rain, now matches 'Rainy Day'
-    assert("rain" in weather.lower())
-
-
-if __name__ == '__main__':
-    testRainy()
-    testHighPrecipitation()
-    print("All is well (maybe!)");
